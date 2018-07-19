e48767ba87ccc2cedee5a6eda1b4749121e2e7a1
Matt Maher <matt@maher-family.net>
2018-07-19 14:39:26 -0700

    - typo

--- aliases_baroness.sh
+++ aliases_baroness.sh
@@ -7,5 +7,5 @@ alias fixed-no='cd bar;npm run start-fixture-noreload'
 alias liveno='cd bar;npm run start-live-noreload'
 alias fixedno='cd bar;npm run start-fixture-noreload'
 
-alias blive-no='live'
+alias blives='live'
 alias bliveno='liveno'
