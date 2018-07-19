456c99d63a193bd616185644fccb8aef7272b80e
Matt Maher <matt@maher-family.net>
2018-07-19 13:48:03 -0700

    - simple aliases

--- aliases_baroness.sh
+++ aliases_baroness.sh
@@ -2,7 +2,10 @@ alias live='cd bar;npm run start-live'
 alias fixed='cd bar;npm run start-fixture'
 
 # no reloaders
-alias live-no='cd bar;npm run start-live-noreload'
+alias live-no='cd bacr;npm run start-live-noreload'
 alias fixed-no='cd bar;npm run start-fixture-noreload'
 alias liveno='cd bar;npm run start-live-noreload'
 alias fixedno='cd bar;npm run start-fixture-noreload'
+
+alias blive-no='live'
+alias bliveno='liveno'
