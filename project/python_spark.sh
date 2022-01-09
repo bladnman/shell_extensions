d26113485d7330bd75efc4e5ada6c0ead303336d
Matt Maher <matt@maher-family.net>
2022-01-09 09:10:53 -0800

    new project for python spark

--- project/python_spark.sh
+++ project/python_spark.sh
@@ -18,6 +18,7 @@ SCRIPT_FULL_PATH_PYTHON_SPARK="$SCRIPT_DIR_PYTHON_SPARK/$(basename -- "$0")"
 # -=  VARS
 # -=-=-=-=-=-=-=-=-=-=-=-=
 CODE_FOLDER_PYTHON_SPARK=~/code/python/python_spark
+CODE_FOLDER_EC2=$CODE_FOLDER_PYTHON_SPARK/ec2
 EC2_INSTANCE_DNS="ec2-18-118-35-218.us-east-2.compute.amazonaws.com"
 
 
@@ -25,7 +26,7 @@ EC2_INSTANCE_DNS="ec2-18-118-35-218.us-east-2.compute.amazonaws.com"
 # -=  ALIASES
 # -=-=-=-=-=-=-=-=-=-=-=-=
 alias cdec2spark='cd $CODE_FOLDER_PYTHON_SPARK'
-alias py_spark_conn_ec2='ssh ubuntu@$EC2_INSTANCE_DNS'
+alias py_spark_conn_ec2='ssh -i $CODE_FOLDER_EC2/ec2_spark.pem ubuntu@$EC2_INSTANCE_DNS'
 
 
 # -=-=-=-=-=-=-=-=-=-=-=-=
